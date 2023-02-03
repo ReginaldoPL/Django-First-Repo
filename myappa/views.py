b0a28cfe51d4c290c3d8952905340bcf06ecafb0
Reginaldo Pedro de Lima <r.pedro.de.lima@avanade.com>
2023-02-03 14:58:35 -0300

    feat: simples get page and form

--- myappa/views.py
+++ myappa/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request): 
-    context = {
-        'name' : 'Patrick',
-        'age': 23,
-        'nationality': 'Brithish',
-        
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
     #return HttpResponse('<h1> Hey, Welcome</h1')
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
