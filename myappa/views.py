1f2fb1897da02c769a17045fa3e08de9bd2aaabf
Reginaldo Pedro de Lima <r.pedro.de.lima@avanade.com>
2023-02-03 14:22:51 -0300

    feat: add context

--- myappa/views.py
+++ myappa/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):    
-    name = 'Patrick'
-    return render(request, 'index.html', {'name': name})
+def index(request): 
+    context = {
+        'name' : 'Patrick',
+        'age': 23,
+        'nationality': 'Brithish',
+        
+    }
+    return render(request, 'index.html', context)
     #return HttpResponse('<h1> Hey, Welcome</h1')
